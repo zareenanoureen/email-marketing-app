0c5f693b868794cdbe42d0a74443de6a1715ae3c
Zareena <zareena.bintenafees@gmail.com>
2024-08-26 16:46:18 +0500

    added iframe path

--- demo_techrealm/urls.py
+++ demo_techrealm/urls.py
@@ -4,5 +4,5 @@ from .views import *
 urlpatterns = [
     path('landing/', index, name='index'),
     path('add-lead/', add_lead, name='add-lead'),
-    path('', lead_iframe, name='add-lead'),
+    path('iframe/', lead_iframe, name='add-lead'),
 ]
